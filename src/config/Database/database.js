6ea5771e469968276307f0e3f481b70526888ea6
Edward Hurtado <edwardhurtado@live.com>
2018-10-28 14:29:15 -0400

    Heroku deploy

--- src/config/Database/database.js
+++ src/config/Database/database.js
@@ -1,6 +1,6 @@
 module.exports={
-    host:"localhost",
-    user:"root",
-    password:"",
+    host:"den1.mysql2.gear.host",
+    user:"exchange",
+    password:"Tm6f02?I9Kx~",
     database:"exchange"
 }
