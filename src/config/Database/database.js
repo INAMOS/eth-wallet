c3c9ae94c1460b5a1f84b1f08ed0dbda9743ecdd
Edward Hurtado <edwardhurtado@live.com>
2018-11-10 18:50:15 -0500

    Fixing errors

--- src/config/Database/database.js
+++ src/config/Database/database.js
@@ -1,6 +1,13 @@
+// module.exports={
+//     host:"den1.mysql2.gear.host",
+//     user:"exchange",
+//     password:"Tm6f02?I9Kx~",
+//     database:"exchange"
+// }
+
 module.exports={
-    host:"den1.mysql2.gear.host",
-    user:"exchange",
-    password:"Tm6f02?I9Kx~",
+    host:"localhost",
+    user:"root",
+    password:"",
     database:"exchange"
 }
